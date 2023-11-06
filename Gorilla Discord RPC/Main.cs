b500e1d93ccc8e92f2267dbf72920d90b20397ef
Chin <86007652+Chin0303@users.noreply.github.com>
2023-11-06 13:03:42 +0100

    Mod Project Setup

---
+++ Gorilla Discord RPC/Main.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+namespace Gorilla_Discord_RPC
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    internal class Main 
+    {
+        internal const string
+            GUID = "chin.discordrpc",
+            NAME = "Gorilla Discord RPC",
+            VERSION = "1.0.0";
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+No idea if i will finish this because this is made out of boredom
