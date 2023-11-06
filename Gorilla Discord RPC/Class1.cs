10d71313a312027ef92b1deb4e9d1b0471adf693
Chin <86007652+Chin0303@users.noreply.github.com>
2023-11-06 12:49:17 +0100

    Add project files.

---
+++ Gorilla Discord RPC/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gorilla_Discord_RPC
+{
+    public class Class1
+    {
+    }
+}
