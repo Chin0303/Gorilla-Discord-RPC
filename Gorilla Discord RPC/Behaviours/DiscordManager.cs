f04fa127c47c71fffac180802e33a8e1773664d0
Elmishhh <agamelmish@gmail.com>
2025-02-12 16:41:20 +0200

    GetGameMode() is no longer hardcoded
    edited GetGetMode() so it's no longer hard-coded

--- Gorilla Discord RPC/Behaviours/DiscordManager.cs
+++ Gorilla Discord RPC/Behaviours/DiscordManager.cs
@@ -40,23 +40,13 @@ namespace Gorilla_Discord_RPC.Behaviours
 
         private string GetGameMode()
         {
-            switch (GorillaComputer.instance.currentGameMode.Value.ToLower())
+            string gameModeRaw = GorillaComputer.instance.currentGameMode.Value;
+            if (gameModeRaw.ToLower().Contains("modded"))
             {
-                case "casual":
-                    return "Playing Casual";
-                case "infection":
-                    return "Playing Infection";
-                case "hunt":
-                    return "Playing Hunt"; // theoretical because hunt isnt a thing right now
-                case "battle":
-                    return "Playing PaintBrawl"; // also not a thing right now
-                case "freezetag":
-                    return "Playing FreezeTag"; // it's just infection but more toxic, still good tho
-                case "guardian":
-                    return "Playing Guardian"; // who the fuck even plays this?
-                default:
-                    return "In A Modded Lobby"; // too lazy to also write down the modded gamemode and it will take up alot of space on the activity
+                return "In a Modded Lobby";
             }
+            string gameMode = char.ToUpper(gameModeRaw[0]) + gameModeRaw.ToLower().Substring(1);
+            return $"Playing {gameMode}";
         }
 
         private string GetRoomState()
